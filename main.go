1c2ff80aa26d4e8ee788559e18ebc8f07eee779f
Gabriel D'Intino <GDintino@proveedor.teco.com.ar>
2019-02-20 15:18:33 -0300

    Variables have been renamed.

--- main.go
+++ main.go
@@ -11,38 +11,36 @@ import (
 func main() {
 	fileName := "./files/SRV_LINEAS.DAT"
 
-	horaInicio := time.Now()
-	content, err := ioutil.ReadFile(fileName)
+	processStartTime := time.Now()
+	fileContent, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		panic(err)
 	}
-	lines := strings.Split(string(content), "\n")
-	originalLines := make([]string, len(lines))
-	copy(originalLines, lines[:])
-	fmt.Println(len(originalLines))
-	fmt.Println(cap(originalLines))
-	// tamaño de chunk
-	chunk := 10000
-	inicio := 0
-	fin := 10000
-	cantidadChunk := len(originalLines) / chunk
+	lineContent := strings.Split(string(fileContent), "\n")
+	lines := make([]string, len(lineContent))
+	copy(lines, lineContent[:])
+	fmt.Println(len(lines))
+	fmt.Println(cap(lines))
+	chunkLen := 10000
+	chunkStart := 0
+	chunkEnd := chunkLen
+	quantityChunk := len(lines) / chunkLen
 	var wg sync.WaitGroup
-	wg.Add(cantidadChunk)
-	for i := 1; i <= cantidadChunk; i++ {
-		//fmt.Println(originalLines[inicio:fin])
-		go sendDataToSDL(originalLines, inicio, fin, &wg)
-		inicio = fin
-		fin = chunk * i
+	wg.Add(quantityChunk)
+	for i := 1; i <= quantityChunk; i++ {
+		go sendDataToSDL(lines, chunkStart, chunkEnd, &wg)
+		chunkStart = chunkEnd
+		chunkEnd = chunkLen * i
 	}
-	fmt.Println(originalLines[fin : len(originalLines)-1])
+	fmt.Println(lines[chunkEnd : len(lines)-1])
 	wg.Wait()
-	horaFin := time.Now()
-	fmt.Println(horaInicio)
-	fmt.Println(horaFin)
-	fmt.Println(cantidadChunk)
+	processEndTime := time.Now()
+	fmt.Println(processStartTime)
+	fmt.Println(processEndTime)
+	fmt.Println(quantityChunk)
 }
 
-func sendDataToSDL(originalLines []string, inicio int, fin int, wg *sync.WaitGroup) {
+func sendDataToSDL(lines []string, chunkStart int, chunkEnd int, wg *sync.WaitGroup) {
 	defer wg.Done()
-	fmt.Println(originalLines[inicio:fin])
+	fmt.Println(lines[chunkStart:chunkEnd])
 }
